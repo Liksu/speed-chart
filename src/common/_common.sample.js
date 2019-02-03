5c6e053517ada8f877b5b3e525d456f3bd660a85
Liksu <github@Liksu.kiev.ua>
2019-02-03 06:15:16 +0200

    fix common sample

--- src/common/_common.sample.js
+++ src/common/_common.sample.js
@@ -1,16 +1,7 @@
 import SpeedChart from "../speed-chart.js";
-import {safeMerge} from "../utils.js";
 
 export default class Common extends SpeedChart {
     static get defaultConfig() {
         return {};
-    }
-
-    constructor(element, settings) {
-        const fixed = SpeedChart.fixParameters(element, settings);
-
-        fixed.settings = safeMerge(Common.defaultConfig, fixed.settings);
-
-        super(fixed.element, fixed.settings);
     }
 }
