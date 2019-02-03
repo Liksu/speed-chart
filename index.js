74cca37b5f5f798661fcd01b054eaa9971d11e3b
Liksu <github@Liksu.kiev.ua>
2019-02-03 06:36:45 +0200

    organize index.js

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-import SpeedChart from './src/speed-chart.js';
+export {default as SpeedChart} from './src/speed-chart.js';
+export {default as AnimatedSpeedChart} from './src/animated-speed-chart.js';
 
-export {SpeedChart};
+export {default as Tree} from './src/tree.js';
+import * as chartUtils from './src/utils.js';
+export {chartUtils};
+
+export {default as ArrowPlugin} from './src/plugins/arrow.js';
+export {default as BackgroundPlugin} from './src/plugins/background.js';
+export {default as GaugePlugin} from './src/plugins/gauge.js';
+export {default as MaskPlugin} from './src/plugins/mask.js';
+export {default as NeedlePlugin} from './src/plugins/needle.js';
+export {default as RainbowPlugin} from './src/plugins/rainbow.js';
+export {default as RedZonePlugin} from './src/plugins/redZone.js';
+export {default as SpeedTextPlugin} from './src/plugins/speed.js';
+export {default as TicksPlugin} from './src/plugins/ticks.js';
+
+export {default as Gauge} from './src/common/gauge.js';
+export {default as Speedometer} from './src/common/speedometer.js';
+export {default as Spinner} from './src/common/spinner.js';
